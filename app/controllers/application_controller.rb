4c49f1f466c7f1c69ef7f2246ef2e19564651dcd
fliblip <wickynjy@gmail.com>
2022-11-16 11:52:59 +0300

    order by price test failing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   # add routes
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get 'baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(price: :DESC)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    max_price = BakedGood.maximum(:price).to_json
+    BakedGood.find_by(price: max_price).to_json
+  end
+
 
 end
